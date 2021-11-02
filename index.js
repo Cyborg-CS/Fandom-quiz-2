f8d1312501a08e7350d88f38e0fd798aadfa67d4
devMainak <81987904+Cyborg-CS@users.noreply.github.com>
2021-11-02 10:09:14 +0000

    feat: added a remark message based on score

--- index.js
+++ index.js
@@ -40,28 +40,34 @@ console.log("----------------");
 questionList = [
   {
     question: "1.What type of drugs Walter & Jesse were producing in the series of Breaking Bad ?) \nA: Cocaine \nB: Marijuana \nC: Crystal Meth \nD: Heroin \nYour Answer: ",
-    answer: "c"
+    answer: "C"
   },{
     question: "2.According to the show Lucifer, what was the Celestial name of Lucifer ? \nA: Samael \nB: Atlas \nC: Nova \nD: Orion \nYour Answer: ",
-    answer: "a"
+    answer: "A"
   },{
-    question: "3.From which political party did Tommy Shelby won the elcetion for MP seat in the series of Peaky Blinders ? \nA: Conservative Party \nB: Communist Party of Britain \nC: Labour Party \nD: Republican Party of Britain \nYour Answer: ",
-    answer: "c"
+    question: "3.From which political party did Tommy Shelby won the elcetion for MP seat in the series of Peaky Blinders ? \nA: Conservative Party \nB: Communist Party of Britain \nC: Labour Party \nD: Liberal Party \nYour Answer: ",
+    answer: "C"
   },{
     question: "4.In the series of 'YOU', who killed Love's brother Forty Quinn ? \nA: Joe \nB: Officer David Fincher \nC: Candis \nD: None \nYour Answer: ",
-    answer: "b"
+    answer: "B"
   },{
     question: "5.How many timelines are actually shown in the series Dark ? \nA: 3 \nB: 2 \nC: 7 \nD: 5 \nYour Answer: ",
-    answer: "d"
+    answer: "D"
   },{
     question: "6.Who played the character 'El Professor' in the series of Money Heist ? \nA: Alvaro Morte \nB: Miguel Angel Silvestre \nC: Mario Casas \nD: Mark Consuelos \nYour Answer: ",
-    answer: "a"
+    answer: "A"
   },{
     question: "7.Who were making a secret base under the super market in Stranger Things Season-3 ? \nA: Chinese \nB: Russians \nC: British \nD: Germans \nYour Answer: ",
-    answer: "b"
+    answer: "B"
   },{
     question: "8.What was the third game for the players of Squid Game ? \nA: Marbel Game \nB: Pass the glass bridge \nC: Squid Game \nD: Tug of war \nYour Answer: ",
-    answer: "d"
+    answer: "D"
+  },{
+    question: "9.Where 'Any Where' key was found in the series of Locke & Key ? \nA: In the Benjamin Locke's potrait \nB: Underneath a bench of Matheson Academy \nC: From Kinsey's bracelet \nD: Inside Erin's mind \nYour Answer: ",
+    answer: "C"
+  },{
+    question: "10.In which year Netflix was founded ? \nA: 1993 \nB: 1997 \nC: 1996 \nD: 1995 \nYour Answer: ",
+    answer: "B"
   }
 ]
 
@@ -73,17 +79,19 @@ for(i = 0; i<questionList.length; i++){
 
 console.log("Your Final Score: "+chalk.red.bold(score))
 
-if(score==="8"){
+if(score==="10"){
   console.log(chalk.yellow.underline("PERFECT ! YOU ARE OFFICIALLY A NETFLIX NERD FROM NOW ON"))
+}else{
+  console.log(chalk.red.bold("NEARLY, PERFECTION ! "))
 }
 
 var highScores = [
   {
     name:"Mainak",
-    score: 8
+    score: 10
   },{
     name:"Sandy",
-    score: 6
+    score: 8
   }
 ]
 
